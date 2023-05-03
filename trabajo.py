19ba4681cf781adb7bd4e41b50d3c555e39ade5f
Ricardoyoly <132397729+Ricardoyoly@users.noreply.github.com>
2023-05-02 19:54:16 -0500

    Add files via upload

---
+++ trabajo.py
@@ -0,0 +1,171 @@
+print("Area de un triangulo")
+
+#entrada
+base = int(input("Ingrese la base del triangulo: "))
+altura = int(input("Ingrese la altura del triangulo: "))
+
+#proceso
+area = (base * altura) / 2
+
+#salida 
+print("El resultado del area es: ", area)
+
+print("Calculadora")
+
+#entrada 
+valor1 = int(input("Ingrese el primer valor: "))
+valor2 = int(input("ingrese el segundo valor: "))
+
+#Proceso
+suma = (valor1 + valor2)
+
+#salida
+print("valor: ", suma)
+
+print("Calculadora al cuadrado")
+
+#entrada
+valor = int(input("Ingrese el valor que desea elevar: "))
+
+#proceso
+elevar = (valor * valor)
+          
+#salida
+print("valor: ", elevar)
+
+print("Suma de 1234 y 532")
+
+#entrada
+num1 = 1234
+num2 = 532
+
+#proceso
+suma = (num1 + num2)
+
+#salida
+print("El resultado de la suma es: ", suma)
+
+print("Resta de 1234 y 532")
+
+#entrada
+num1 = 1234
+num2 = 532
+
+#proceso
+resta = (num1 - num2)
+
+#salida
+print("El resultado de la resta es: ", resta)
+
+print("Multiplicacion de 1234 y 532")
+
+#entrada
+num1 = 1234
+num2 = 532
+
+#proceso
+multi = (num1 * num2)
+
+#salida
+print("El resultado de la multiplicacion es: ", multi)
+
+print("Division de 1234 y 532")
+
+#entrada
+num1 = 1234
+num2 = 532
+
+#proceso
+div = (num1 / num2)
+
+#salida
+print("El resultado de la division es: ", div)
+
+print("Porcentaje de 1234 y 532")
+
+#entrada
+num1 = 1234
+num2 = 532
+
+#proceso
+porcent = (num1 % num2)
+
+#salida
+print("El resultado del porcentaje es: ", porcent)
+
+print("Convertir euros a dolares")
+
+#entrada
+euros = float(input("Ingrese la cantidad que desea covertir: "))    
+
+#proceso
+num = 1.10  
+dolares = (euros * num)
+
+#salida
+print("La convercion a dolares es: ", dolares)
+
+print("Carcular el area de un rectangulo")
+
+#entrada
+largo = float(input("Ingrese el largo de la figura: "))
+ancho = float(input("Ingrese el archo de la figura: "))
+
+#proceso
+area = (largo * ancho)
+
+#salida
+print("El area de tu figura es: ", area)
+
+print("Calcular area y perimetro de un cuadrado")
+
+#entrada
+lado = int(input("Ingrese el valor del lado de su cuadrado: "))
+
+#proceso
+area = (lado * lado)
+perim= (4 * lado)
+
+#salida
+print("El resultado del area es: ", area)
+print("El resultado del perimetro es: ", perim)
+
+print("Area y voluen de un cilindro")
+
+#entrada
+altura = float(input("Ingrese el valor de la altura del cilindro: "))
+radio = float(input("Ingrese el valor del radio de su cilindro: "))
+
+#proceso
+area = (2 * 3.1416 * radio * (radio + altura))
+vol = (3.1416 * radio ** 2 * altura)
+
+#salida
+print("El area del cilindro es: ", area)
+print("El volumen del cilindro es: ", vol)
+
+print("Longitud y Area")
+#entrada 
+import math 
+radio = float(input("Ingrese la medida del radio: "))
+
+#proceso 
+longitud = 2 * math.pi * radio
+area = math.pi * radio ** 2
+
+#salida 
+print(" El resultado de la longitud es: ", longitud)
+print(" El resultado del area es: ", area)
+
+print("Promedio")
+
+#entrada
+num1 = float(input("Ingrese el primer valor: "))
+num2 = float(input("Ingrese el segundo valor: "))
+num3 = float(input("Ingrese el tercer valor: "))
+
+#proceso 
+promedio = (num1 + num2 + num3) / 3
+
+#salida
+print("El promedio de los valores es: ", promedio)
